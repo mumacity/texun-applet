4410c7698cac9382c4ff9f64be484f32fe1e862b
mumacity <buqingyun@mumacitydeMac-mini.local>
2018-10-22 21:07:54 +0800

    官网v1.0.0

---
+++ v1.0.0/data.js
@@ -0,0 +1,186 @@
+let database = [
+  {
+    caseInfo: [
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/1.png',
+        bigImg: ['http://www.texunkeji.net/pic/1s.jpg',],
+        title: '中捷尼特拉',
+        caseid: '0'
+      },
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/2.png',
+        bigImg: ['http://www.texunkeji.net/pic/2s.jpg'],
+        title: '禹王防水',
+        caseid: '1'
+      },
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/3.png',
+        bigImg: ['http://www.texunkeji.net/pic/3s.jpg'],
+        title: '恒普金融',
+        caseid: '2'
+      },
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/4.png',
+        bigImg: ['http://www.texunkeji.net/pic/4s.jpg'],
+        title: '凯隆电器',
+        caseid: '3'
+      },
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/5.png',
+        bigImg: ['http://www.texunkeji.net/pic/5s.jpg'],
+        title: '优飞科技',
+        caseid: '4'
+      },
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/6.png',
+        bigImg: ['http://www.texunkeji.net/pic/6s.jpg'],
+        title: '用友科技',
+        caseid: '5'
+      },
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/7.png',
+        bigImg: ['http://www.texunkeji.net/pic/71.png', 'http://www.texunkeji.net/pic/72.png','http://www.texunkeji.net/pic/73.png'],
+        title: 'Health Type',
+        caseid: '6'
+      },
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/8.png',
+        bigImg: ['http://www.texunkeji.net/pic/81.png', 'http://www.texunkeji.net/pic/82.png','http://www.texunkeji.net/pic/83.png'],
+        title: '随心旅行',
+        caseid: '7'
+      },
+      {
+        imgSrc: 'http://www.texunkeji.net/pic/9.png',
+        bigImg: ['http://www.texunkeji.net/pic/91.png', 'http://www.texunkeji.net/pic/92.png','http://www.texunkeji.net/pic/93.png'],
+        title: '朝夕便签',
+        caseid: '8'
+      }
+    ],
+    partner: [
+      'http://www.texunkeji.net/pic/company1.png',
+      'http://www.texunkeji.net/pic/company2.png',
+      'http://www.texunkeji.net/pic/company3.png',
+      'http://www.texunkeji.net/pic/company4.png',
+      'http://www.texunkeji.net/pic/company5.png',
+      'http://www.texunkeji.net/pic/company6.png',
+      'http://www.texunkeji.net/pic/company7.png',
+      'http://www.texunkeji.net/pic/company8.png'
+    ],
+    imgUrls: [
+      'http://www.texunkeji.net/pic/banner1.png',
+      'http://www.texunkeji.net/pic/banner2.png',
+      'http://www.texunkeji.net/pic/banner4.png',
+      'http://www.texunkeji.net/pic/banner5.png'
+    ]
+  },
+  {
+    introduction: [
+     {
+        'imgSrc': 'http://www.texunkeji.net/pic/range1.jpg',
+        'bigImg': ['http://www.texunkeji.net/pic/range1.jpg', 'http://www.texunkeji.net/pic/range2.jpg', 'http://www.texunkeji.net/pic/range3.png','http://www.texunkeji.net/pic/range4.png']
+     },
+     {
+       'imgSrc': 'http://www.texunkeji.net/pic/range2.jpg',
+       'bigImg': ['http://www.texunkeji.net/pic/range2.jpg', 'http://www.texunkeji.net/pic/range3.png', 'http://www.texunkeji.net/pic/range4.png','http://www.texunkeji.net/pic/range1.jpg']
+     },
+     {
+       'imgSrc': 'http://www.texunkeji.net/pic/range3.png',
+       'bigImg': ['http://www.texunkeji.net/pic/range3.png', 'http://www.texunkeji.net/pic/range4.png', 'http://www.texunkeji.net/pic/range1.jpg','http://www.texunkeji.net/pic/range2.jpg']
+     },
+     {
+       'imgSrc': 'http://www.texunkeji.net/pic/range4.png',
+       'bigImg': ['http://www.texunkeji.net/pic/range4.png', 'http://www.texunkeji.net/pic/range1.jpg', 'http://www.texunkeji.net/pic/range2.jpg','http://www.texunkeji.net/pic/range3.png']
+     }
+    ]
+  },
+  {
+    dynamic: [
+      {
+        dynamicTitle: "工建通APP",
+        postImgSrc: "http://www.texunkeji.net/pic/a1.jpg",
+        postTime: "2018-10-18 14:26:54",
+        postid: 0,
+        text:[
+            ''
+        ],
+         detailSrc: [
+          'http://www.texunkeji.net/pic/a2.jpg',
+          'http://www.texunkeji.net/pic/a3.jpg',
+          'http://www.texunkeji.net/pic/a4.jpg'
+        ]
+      },
+      {
+        dynamicTitle: "投资一年，获利200万",
+        postImgSrc: "http://www.texunkeji.net/pic/b1.png",
+        postTime: "2018-08-14 10:33:36",
+        postid: 1,
+        text: [
+          ''
+        ],
+        detailSrc: [
+          'http://www.texunkeji.net/pic/b2.jpg'
+        ]
+      },
+      {
+        dynamicTitle: "应急维修",
+        postImgSrc: "http://www.texunkeji.net/pic/c1.jpg",
+        postTime: "2018-03-26 15:14:20",
+        postid: 2,
+        text: [
+          ''
+        ],
+        detailSrc: [
+          'http://www.texunkeji.net/pic/c2.jpg',
+          'http://www.texunkeji.net/pic/c4.jpg',
+          'http://www.texunkeji.net/pic/c3.jpg'
+
+        ]
+      },
+      {
+        dynamicTitle: "黑龙江省市领导来特讯科技参观视察",
+        postImgSrc: "http://www.texunkeji.net/pic/f1.jpg",
+        postTime: "2018-03-26 15:14:20",
+        postid: 3,
+        text: [
+          '黑龙江省商局局长李延安、处长李颜红、处长刁修广和市里区里领导、来特讯科技参观调研，特讯科技董事长张红波讲解了自主开发的《工建通》，涉及工程领域一站式服务平台、弥补行业在互联网上的空白，解决行业中的痛点，迎合了互联网+的趋势，也将在几年内打造全国工程行业中旗舰，树立电商中的亮点，也将会实现产业中的独角兽。',
+          '工建通APP项目得到省市领导的高度认可，并发言表示工建通项目在全省列为重点关注和大力支持项目。'
+        ],
+        detailSrc: [
+          'http://www.texunkeji.net/pic/f2.jpg',
+          'http://www.texunkeji.net/pic/f3.jpg',
+          'http://www.texunkeji.net/pic/f4.jpg'
+        ]
+      },
+      {
+        dynamicTitle: "哈尔滨市委书记王兆力来特讯科技视察",
+        postImgSrc: "http://www.texunkeji.net/pic/d1.JPG",
+        postTime: "2017-10-10 09:30:28",
+        postid: 4,
+        text: [
+          '昨天下午，省委常委、市委书记王兆力到华南城调研项目建设情况，帮助企业协调解决在建设中遇到的问题。王兆力强调，当前，要持续聚焦经济建设这个中心，在抓产业项目上狠下功夫，勇于担当，攻坚克难，全力支持、强力推进项目建设，不断提高全市经济发展的质量和效益。市委常委、秘书长王文力参加调研，走访过程中针对黑龙江特讯科技有限责任公司进行了解，特讯科技董事长张红波对新开发出的产品“工建通”APP向王兆力书记进行详细讲解，提到迎合趋势的发展，弥补行业上的空白，解决专业上的痛点，建立线上线下推广体系，实现电商业的一个热点。谈话过程中王兆力书记询问了工建通APP的详细功能，上线时间，开发团队和推广问题。',
+          '工建通APP涉及工程领域一站式服务平台、弥补行业在互联网上的空白，解决行业中的痛点，迎合了互联网+的趋势，也将在几年内打造全国工程行业中旗舰，树立电商中的亮点，也将会实现产业中的独角兽。',
+          '哈尔滨市委常委、秘书长王文力、哈尔滨市交通局局长李庆、道外区委书记马旦曰，哈尔滨市农委副主任高洪大、哈尔滨市住建委副主任全强等领导陪同调研。',
+          '工建通APP项目得到省市领导的高度认可，并发言表示工建通项目在全省列为重点关注和大力支持项目。'
+        ],
+        detailSrc:[
+          'http://www.texunkeji.net/pic/d2.JPG',
+          'http://www.texunkeji.net/pic/d3.JPG'
+        ]
+      }
+    ]
+  },
+  {
+    aboutImg: 'http://www.texunkeji.net/pic/about.jpg',
+    aboutText: [
+      '黑龙江特讯科技有限责任公司是一家创业型公司，年轻、富有活力，专业从事于APP开发建设、推广和网络营销的互联网基础运行商公司成立于2017年5月10日，现有员工20人，公司注册资金500万元。',
+      '公司致力于工程建筑行业电子商务的应用和推广拥有独立的高端服务区虚拟主机、中文网址、网站建设、网站推广、网络营销和 软件开发的企业上网全程服务。',
+      '我们从市场的角度和客户的需求出发，帮助企业及其产品树立良好的视觉形象，拓展市场空间创造竞争优势，提升企业的无形资产。',
+      '我们亦明知时代赋予设计师的伟大抱负和责任，须以前瞻性的眼光和精致的作品引导社会，提升民众的审美品味和生活品质。'
+    ]
+  }
+];
+
+
+module.exports = {
+    postList: database
+}
